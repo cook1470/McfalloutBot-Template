f8eb467da5cdaa9359303622f8f40f8c541710be
cook1470 <b43096022@gmail.com>
2024-11-09 03:23:01 +0800

    添加 static DEPENDENCIES 可設置前置/依賴插件

--- src/bots/plugins/BasePlugin.ts
+++ src/bots/plugins/BasePlugin.ts
@@ -7,7 +7,20 @@ export class BasePlugin {
     /** 插件類別，每個繼承此類別的插件都需要定義自己的 TYPE。 */
     static TYPE: string = 'BasePlugin';
 
+    /**
+     * 依賴插件，若本插件有使用到其他插件的功能，可以考慮添加，會在插件創建時自動檢查並創建依賴插件，若依賴插件已經存在於 BaseBot 則略過。
+     * 
+     * 當然也可以直接在 BaseBot 內就創建好全部的插件，這只是一種保險起見。
+     */
+    static DEPENDENCIES: (typeof BasePlugin)[] = [];
+
     constructor(private _baseBot: BaseBot) {
+
+        const MyClass = this.constructor as typeof BasePlugin;
+
+        for (const PluginClass of MyClass.DEPENDENCIES) {
+            if (!_baseBot.getPlugin(PluginClass.TYPE) && PluginClass !== MyClass) _baseBot.loadPlugin(new PluginClass(_baseBot));
+        }
 
     }
 
